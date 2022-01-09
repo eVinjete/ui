85045447f8e729fd8cd842f4e941038dab33beb5
lovrovrazic <lovro.vrazic@gmail.com>
2022-01-09 19:28:43 +0100

    register user work

--- src/main/java/si/evinjete/ui/UiBean.java
+++ src/main/java/si/evinjete/ui/UiBean.java
@@ -1,17 +1,74 @@
 package si.evinjete.ui;
 
+import si.evinjete.uporabniki.Uporabnik;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
 import java.io.Serializable;
 
 public class UiBean implements Serializable {
+
+    WebTarget wb;
+
+    public UiBean(){}
 
     String name;
     String surname;
     String email;
     String type;
 
-    public UiBean(){}
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 
     public void registerUser() {
 
+        Uporabnik uporabnik = new Uporabnik();
+        uporabnik.setName(this.name);
+        uporabnik.setSurname(this.surname);
+        uporabnik.setEmail(this.email);
+        uporabnik.setType(Integer.parseInt(this.type));
+
+        Client client = ClientBuilder.newClient();
+        wb = client.target("http://uporabniki-service.default.svc.cluster.local:8080/v1/");
+        String response = wb.request(MediaType.APPLICATION_JSON).post(Entity.json(uporabnik), String.class);
+
+        System.out.println("INFO -- user " + response + " registered. ");
+
+        this.name = null;
+        this.surname = null;
+        this.email = null;
+        this.type = null;
     }
 }
